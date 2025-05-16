70bb529084e6c0edfddc9d421c6e5d851bd93f50
Jonathan Kelvin <jooh.kelvin@gmail.com>
2025-05-15 21:53:47 -0300

    Feat: Conteúdo Parte 01

---
+++ exercicios/calculadora_soma.py
@@ -0,0 +1,6 @@
+#Calculadora de soma
+
+numero1 = float(input("Digite o primeiro número: "))
+numero2 = float(input("Digite o segundo número: "))
+numero3 = numero1 + numero2
+print(f"A soma do {numero1} e {numero2} é: {numero3}")
